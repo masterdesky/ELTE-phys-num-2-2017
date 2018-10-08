926bba45172c2a54691ed7d8d6f35f2d1d8a8542
masterdesky <masterdesky@gmail.com>
2018-10-08 20:39:22 +0200

    cosmetics of task1 - gcc compiled exe

--- Task 1/num_meth_1.c
+++ Task 1/num_meth_1.c
@@ -9,12 +9,11 @@ const char FILE_NAME_VECTOR[] = "vector.txt";
 
 
 // Allocating memory for the various array objects
-double* AllocateMemory(double* Tensor)
+double* AllocateMemory(double* InputTensor)
 {
+    InputTensor = (double*)calloc(16, sizeof(double));
 
-    Tensor = (double*)calloc(16, sizeof(double));
-
-    return Tensor;
+    return InputTensor;
 }
 
 // Reading in a Tensor from an input file
@@ -26,12 +25,19 @@ double* AllocateMemory(double* Tensor)
 // should have the same amount of numbers. (Imperfect rows/columns are permitted.)
 double* ReadInTensor(FILE* Input, double* Tensor, int* NumberOfRows)
 {
+    // Allocate memory for input tensor
+    Tensor = AllocateMemory(Tensor);
+
+    // Temporary storage for numbers in a chararray
     char FileStream;
     char* CurrentNumber = (char*)calloc(16, sizeof(char));
 
+    // Index for elements of the current char array
     int CharIndex = 0;
+    // Index for counting elements of output tensor
     int TensorIndex = 0;
 
+    // Index for tensor elements
     double CurrentTensorElement;
 
     while(1)
@@ -109,91 +115,6 @@ double* ReadInTensor(FILE* Input, double* Tensor, int* NumberOfRows)
 }
 
 
-// Reading in 1D array from an input file
-// The file could contain doubles, with '.' or ',' as decimal points.
-// Between the doubles any kind of denumerator is allowed, as long
-// as they're not the '.', or ',' (decimal points) or the '-' (minus sign) characters.
-//
-// Here, the file shouldn't "look like" a 1D array. All of the numbers will be
-// read and appended to the output array.
-double* ReadInOneDArray(FILE* Input, double* OneDArray)
-{
-    char FileStream;
-    char* CurrentNumber = (char*)calloc(16, sizeof(char));
-
-    int CharIndex = 0;
-    int ElementIndex = 0;
-
-    double CurrentOneDArrayElement;
-
-    while(1)
-    {
-        // Read characters one by one from Input file to FileStream char 
-        FileStream = getc(Input);
-
-        // If EOF is reached, then append the number in the CurrentNumber buffer to 
-        // the OneDArray array, then break the loop.
-        if(FileStream == EOF)
-        {
-            // Null terminating string
-            CurrentNumber[CharIndex] = '\0';
-
-            // Convert string to double
-            CurrentOneDArrayElement = atof(CurrentNumber);
-
-            // Append double to our Tensor
-            OneDArray[ElementIndex] = CurrentOneDArrayElement;
-
-            printf("EOF reached!\n");
-
-            break;
-        }
-
-        // If the current char from the input file is a number, or a decimal point/ +- sign, then
-        // append it to the CurrentNumber char array buffer
-        if((FileStream >= '0' && FileStream <='9') || FileStream == '.' || FileStream == ',' || FileStream == '-')
-        {
-            CurrentNumber[CharIndex] = FileStream;
-            CharIndex += 1;
-
-            if(CharIndex >= (sizeof(CurrentNumber)/sizeof(CurrentNumber[0])))
-            {
-                CurrentNumber = (char*)realloc(CurrentNumber, ((sizeof(CurrentNumber)/sizeof(CurrentNumber[0])) * 2));
-            }
-        }
-
-        // If it isn't, then null terminate the CurrentNumber buffer, convert it to integer,
-        // and then append it to the OneDArray double* array
-        else
-        {
-             // Null terminating string
-            CurrentNumber[CharIndex] = '\0';
-
-            // Convert string to double
-            CurrentOneDArrayElement = atof(CurrentNumber);
-
-            // Append double to our OneDArray
-            OneDArray[ElementIndex] = CurrentOneDArrayElement;
-
-            // Relaunch indexing for a new 1D array element
-            CharIndex = 0;
-
-            // Temp. counter for elements of the 1D array
-            ElementIndex += 1;
-
-            if(ElementIndex >= (sizeof(OneDArray)/sizeof(OneDArray[0])))
-            {
-                OneDArray = (double*)realloc(OneDArray, ((sizeof(OneDArray)/sizeof(OneDArray[0])) * 2));
-            }
-        }
-    }
-
-    free(CurrentNumber);
-
-    return OneDArray;
-}
-
-
 // Checking if the matrix and the vector have correct shapes
 //
 // To broadcast a matrix and vector together, the vector's lenght should be
@@ -265,13 +186,11 @@ int main()
     double* Vector;
 	double* Output;
 
-    Matrix = AllocateMemory(Matrix);
-    Vector = AllocateMemory(Vector);
-
     //printf("OK");
 
     // Index for counting rows of our matrix
     int NumberOfRowsMx = 1;
+    int NumberOfRowsVc = 0; // NOT USED, ONLY AUXILIARY
 
     // ================== Matrix
     // Reading in matrix from a file to the Matrix double* array
@@ -304,7 +223,7 @@ int main()
 
     // Read in vectorfrom input file
     // The ReadInOneDArray function returns a double* array
-    Vector = ReadInOneDArray(Input_Vc, Vector);
+    Vector = ReadInTensor(Input_Vc, Vector, &NumberOfRowsVc);
 
     // Close the input file
     fclose(Input_Vc);
@@ -315,24 +234,14 @@ int main()
 
     // Calculate constants for this particular problem
     // Size of the vector
-    int NumberOfRowsVc = sizeof(Vector) / sizeof(Vector[0]);
+    NumberOfRowsVc = sizeof(Vector) / sizeof(Vector[0]);
     // Size of the full matrix
     int SizeOfMatrix = sizeof(Matrix) / sizeof(Matrix[0]);
     // Column length of the matrix
     int NumberOfColumnsMx = SizeOfMatrix / NumberOfRowsMx;
 
-    printf("sizeof(Matrix): %llu\n", sizeof(Matrix));
-    printf("Matrix[4]: %lf ", Matrix[4]);
-
-    printf("NumberOfRowsMx: \n%d\n", NumberOfRowsMx);
-    printf("SizeOfMatrix: %d\n", SizeOfMatrix);
-    printf("NumberOfColumnsMx: %d\n", NumberOfColumnsMx);
-    printf("NumberOfRowsVc: %d\n", NumberOfRowsVc);
-
     // Multiply the matrix and the vector if it's possible
 	Output = MultiplyThings(NumberOfRowsMx, NumberOfColumnsMx, NumberOfRowsVc, Matrix, Vector, Output);
-
-    printf("OKout\n");
 
     free(Matrix);
     free(Vector);
