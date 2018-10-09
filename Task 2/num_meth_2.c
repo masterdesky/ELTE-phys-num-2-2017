cc24d8555894e12884afe376e9b16b8eb006d23b
masterdesky <masterdesky@gmail.com>
2018-10-10 01:09:18 +0200

    updating with task 2

--- Task 2/num_meth_2.c
+++ Task 2/num_meth_2.c
@@ -6,24 +6,33 @@
 
 const char FILE_NAME[] = "matrix.txt";
 
-const float floateps = 1.1920929e-07;
+const double floateps = 1.1920929e-07;
 
 
 // Count the rows of the matrix in the input file
-int CountDimensionOfMatrix(FILE* InputFile)
+unsigned int CountDimensionOfMatrix(FILE* InputFile)
 {
     // Start from 1, thus the last line of the file got also included
     // It is needed, because the while+getc beloew at EOF breaks the loop automatically
-    int Counter = 1;
+    unsigned int Counter = 0;
 
     // Char variables to store characters of the file
     // getc() reads characters from the file into the CharacterCurrent variable
     // CharacterPrevious stores the previusly read character until the next step of the loop
-    char CharacterPrevious;
-    char CharacterCurrent = getc(InputFile);
+    char CharacterPrevious = '\0';
+    char CharacterCurrent;
 
-    while(CharacterCurrent != EOF)
+    while(1)
     {
+        CharacterCurrent = getc(InputFile);
+
+        if(CharacterCurrent == EOF)
+        {
+            Counter++;
+            break;
+        }
+        
+
         if(CharacterCurrent == '\n')
         {
             // If there is a double line break ('\n\n'), we don't need to count
@@ -36,7 +45,7 @@ int CountDimensionOfMatrix(FILE* InputFile)
             // Increment the counter by 1, if a new row is found
             else
             {
-                Counter += 1;
+                Counter++;
             }
         }
 
@@ -63,6 +72,7 @@ double* ReadInMatrix(FILE* InputFile, unsigned int DimensionOfMatrix)
     double* InputMatrix = AllocateMemory(DimensionOfMatrix * DimensionOfMatrix);
 
     // Scan in and print out matrix to the standard output
+    printf("Matrix read in:\n");
     for(unsigned int i = 0; i < DimensionOfMatrix; i++)
     {
         for(unsigned int j = 0; j < DimensionOfMatrix; j++)
@@ -73,8 +83,7 @@ double* ReadInMatrix(FILE* InputFile, unsigned int DimensionOfMatrix)
         printf("\n");
     }
 
-    // Close the input file
-    fclose(InputFile);
+    printf("\n");
 
     return InputMatrix;
 }
@@ -110,6 +119,7 @@ double* CreateIdentityMatrix(unsigned int DimensionOfMatrix)
 // While the lefthand part will be a matrix, read from the input file
 double* CreateFullMatrix(FILE* InputFile, unsigned int DimensionOfMatrix)
 {
+
     // Allocate memory for parts of the matrix
     double* FullMatrix = AllocateMemory(2 * DimensionOfMatrix * DimensionOfMatrix);
     double* InputMatrix = AllocateMemory(DimensionOfMatrix * DimensionOfMatrix);
@@ -117,6 +127,21 @@ double* CreateFullMatrix(FILE* InputFile, unsigned int DimensionOfMatrix)
 
     // Input matrix is read from a file by the ReadInmatrix function
     InputMatrix = ReadInMatrix(InputFile, DimensionOfMatrix);
+
+    // Close the input file
+    fclose(InputFile);
+
+    printf("Matrix cloned:\n");
+    for(unsigned int i = 0; i < DimensionOfMatrix; i++)
+    {
+        for(unsigned int j = 0; j < DimensionOfMatrix; j ++)
+        {
+            printf("%lf ", InputMatrix[DimensionOfMatrix * i + j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
 
     // Identity matrix is arbitrarly created
     IdentityMatrix = CreateIdentityMatrix(DimensionOfMatrix);
@@ -151,55 +176,56 @@ double* CreateFullMatrix(FILE* InputFile, unsigned int DimensionOfMatrix)
 // 2. In the next steps the function do the same for the other columns too, starting the loop at the second row, then the third, then so on.
 // 3. After a step of the loop, the function are normalizing the whole row and performing substitutions to form the required inverse matrix.
 // 4. At the end, the diagonal elements will be all just ones, and below the diagonal line will be full of zeros.
-double* GaussJordan(FILE* Input, unsigned int DimensionOfMatrix)
+double* GaussJordan(FILE* InputFile, unsigned int DimensionOfMatrix)
 {
     double* FullGaussMatrix = AllocateMemory(2 * DimensionOfMatrix * DimensionOfMatrix);
 
     // Create the matrix, which we'll work on
-    FullGaussMatrix = CreateFullMatrix(Input, DimensionOfMatrix);
+    FullGaussMatrix = CreateFullMatrix(InputFile, DimensionOfMatrix);
 
     // Temporary storage for indeces
-    // Indicating rows with current greatest element, and newly found row
-    // with even greater element
-    int TempStoreFirst;
-    int TempStoreSecond;
+    // Indicating the index of the row with current greatest element
+    unsigned int TempStoreIndex;
+
+    // Temporary storage for a double element of the matrix
+    double TempStorage;
 
     // Temporary index for matrix elements at normalization
-    int mat_ij;
+    double mat_ij;
     
     // Run column-wise through elements
     // The index (j) here indicates columns
     for(unsigned int j = 0; j < DimensionOfMatrix; j++)
     {
         // Set the jth element of the jth column is initially the greatest element
-        TempStoreFirst = j;
+        TempStoreIndex = j;
 
-        // Search for the greatest element in the jth column, starting from the jth row
-        // it runs through all elements
-        for(unsigned int i = j + 1; j < DimensionOfMatrix; j++)
+        // Search for the greatest element in the jth column, starting from the (j+1)th row
+        // It runs through all elements
+        for(unsigned int i = (j + 1); j < DimensionOfMatrix; j++)
         {
-            if(FullGaussMatrix[DimensionOfMatrix * i + j] > FullGaussMatrix[DimensionOfMatrix * TempStoreFirst + j])
+            if(FullGaussMatrix[DimensionOfMatrix * i + j] > FullGaussMatrix[DimensionOfMatrix * TempStoreIndex + j])
             {
-                TempStoreFirst = j;
+                TempStoreIndex = i;
             }
         }
 
         // Check if the greatest element is smaller, than the 32bit float machine epsilon
         // If yes, then the matrix is numerically singular, and cannot be inverted
-        if((FullGaussMatrix[DimensionOfMatrix * TempStoreFirst + j]) < floateps)
+        if((FullGaussMatrix[DimensionOfMatrix * TempStoreIndex + j]) < floateps)
         {
             printf("Given matrix is singular and cannot be inverted! The program exits.");
             exit(0);
         }
 
         // Swapping rows, which has the greatest element in the column
-        if(TempStoreFirst != j)
+        if(TempStoreIndex != j)
         {
             for(unsigned int k = 0; k < 2 * DimensionOfMatrix; k++)
             {
-                TempStoreSecond = FullGaussMatrix[DimensionOfMatrix * j + k];
-                FullGaussMatrix[DimensionOfMatrix * j + k] = FullGaussMatrix[DimensionOfMatrix * TempStoreFirst + k];
-                FullGaussMatrix[DimensionOfMatrix * TempStoreFirst + k] = TempStoreSecond;
+                TempStorage = FullGaussMatrix[DimensionOfMatrix * j + k];
+                FullGaussMatrix[DimensionOfMatrix * j + k] = FullGaussMatrix[DimensionOfMatrix * TempStoreIndex + k];
+                FullGaussMatrix[DimensionOfMatrix * TempStoreIndex + k] = TempStorage;
             }
         }
 
@@ -251,10 +277,6 @@ void GaussEliminationFunctions()
     // Variable for containing the size (rank) of the matrix
     unsigned int DimensionOfMatrix;
 
-    // double array, to contain the inverse matrix
-    double* FullGaussMatrix = AllocateMemory(2 * DimensionOfMatrix * DimensionOfMatrix);
-
-
     // Open input file
     // It should contain a square matrix with double-point elements
     FILE* InputFile = fopen(FILE_NAME, "r");
@@ -266,9 +288,11 @@ void GaussEliminationFunctions()
         EXIT_FAILURE;
     }
 
-
     // Count dimension of matrix
     DimensionOfMatrix = CountDimensionOfMatrix(InputFile);
+
+    // double array, to contain the inverse matrix
+    double* FullGaussMatrix = AllocateMemory(2 * DimensionOfMatrix * DimensionOfMatrix);
 
     // Inverting matrix
     FullGaussMatrix = GaussJordan(InputFile, DimensionOfMatrix);
@@ -277,8 +301,6 @@ void GaussEliminationFunctions()
     PrintInverseMatrix(FullGaussMatrix, DimensionOfMatrix);
 
     free(FullGaussMatrix);
-    
-    exit(0);
 }
 
 
@@ -307,9 +329,9 @@ int main()
 
             case 'n':
             case 'N':
-                exit(0);
+                break;
         }
-        printf("\n");
+        break;
     }
 
     return 0;
